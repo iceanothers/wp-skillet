f5d6cef1f12f91920c511aabc40cc84d9a2a749d
iceanothers <iceanothers@gmail.com>
2015-12-04 15:44:28 +0200

    111

--- header.php
+++ header.php
@@ -10,10 +10,12 @@ ob_start('ob_html_compress');
 <head>
 	<meta charset="UTF-8">
     <title><?php seo_title(); ?></title>
+    <meta name="MobileOptimized" content="width" />
     <meta name="HandheldFriendly" content="True"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
-    <meta name="apple-mobile-web-app-capable" content="yes"/>
-    <link rel="shortcut icon" href="<?php theme('favicon.ico'); ?>" type="image/x-icon"/>
+    <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="//s1.wp.com/i/favicon.ico?m=1311976025g" sizes="16x16 32x32 48x48">
+    <link rel="shortcut icon" type="image/x-icon" href="//s1.wp.com/i/favicon.ico?m=1311976025g" sizes="16x16">
+    <link rel="icon" type="image/x-icon" href="//s1.wp.com/i/favicon.ico?m=1311976025g" sizes="16x16">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php get_header(); ?>
 <section class="content">
     <div class="container">
+        <aside>
+
+        </aside>
+        <article>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
+            <div class="post">
+                <?php if (has_post_thumbnail()) { ?>
+                    <figure>
+                        <?php the_post_thumbnail(); ?>
+                    </figure>
+                    <div class="info">
+                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <time datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php echo get_the_date(get_option('date_format_custom')); ?></time>
+                        <?php echo short_text($post_id, 50); ?>
+                        <a class="read" href="<?php the_permalink(); ?>">Read more ></a>
+                    </div>
+                <?php } ?>
+            </div>
         <?php endwhile; endif; ?>
+        </article>
     </div>
 </section>
 <?php get_footer(); ?>
