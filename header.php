0f4d70faa1491dbb8d740ef5d2e5066f1a1e6b24
iceanothers <iceanothers@gmail.com>
2015-12-23 09:47:23 +0200

    111

--- header.php
+++ header.php
@@ -24,7 +24,7 @@ header('X-UA-Compatible: IE=Edge');
             <a id="logo" href="<?php echo get_option('home') ?>">
                 <img src="<?php theme('images/logo.png'); ?>" alt="logo">
             </a>
-            <div id="menuOpen">Menu <span></span></div>
+            <div id="menuOpen"><p>Menu</p><span></span></div>
             <nav id="mainMenu">
                 <?php wp_nav_menu(array('container' => false, 'items_wrap' => '<ul id="%1$s">%3$s</ul>', 'theme_location'  => 'main_menu')); ?>
             </nav>
