fffade9b13f7fa812c86da6137f4b81c2bc4c04f
Sergiy Yudintsev <iceanothers@gmail.com>
2016-05-17 14:47:24 +0300

    222

--- functions.php
+++ functions.php
@@ -263,4 +263,15 @@ if (class_exists('Wp_Scss_Settings')) {
     }
 }
 
+function cc_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+function wpa_fix_svg_thumb() {
+    echo '<style>td.media-icon img[src$=".svg"], img[src$=".svg"].attachment-post-thumbnail {width: 100% !important;height: auto !important}</style>';
+}
+add_action('admin_head', 'wpa_fix_svg_thumb');
+
 ?>
